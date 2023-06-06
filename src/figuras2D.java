0e2304e16c4339ecd6135aafb7efeefa37f7981a
Paul Hidalgo <paulesh5@gmail.com>
2023-06-06 07:32:28 -0500

    Creacion de clases y constructor, sett & gett del número de lados

---
+++ src/figuras2D.java
@@ -0,0 +1,16 @@
+public class figuras2D {
+    int nLados;//número de lados
+
+    public figuras2D(int nLados) {
+        this.nLados = nLados;
+    }
+
+    public int getnLados() {
+        return nLados;
+    }
+
+    public void setnLados(int nLados) {
+        this.nLados = nLados;
+    }
+
+}
